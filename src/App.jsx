3808e731dfcc4ae39fd4bd8d2357e30a2a5fb9bf
codeStackr <rdzaman20000@gmail.com>
2024-05-19 22:53:47 +0600

    Fix linters errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-   return (
+  return (
     <>
-      <h1 className='color-blue w-45 bg-indigo p-3 text-red'>Hello World</h1>
+      <h1 className="color-blue w-45 bg-indigo p-3 text-red">Hello World</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';// eslint-enable import/no-extraneous-dependencies
+import react from '@vitejs/plugin-react';// eslint-enable import/no-extraneous-dependencies
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
