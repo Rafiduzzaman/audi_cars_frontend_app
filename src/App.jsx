1e35097b9cf2460ddadcafd8bf52f3f1933679c7
CobbyElsonfx <andohfrancis9187@gmail.com>
2024-06-06 03:13:08 +0000

    adde new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-// import Header from './Components/header.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddVehicleForm from './Components/Forms/addVehicleForm';
+import LoginForm from './Components/Forms/loginForm';
+import SignupForm from './Components/Forms/signupForm';
 import './App.css';
 
 function App() {
   return (
-    <>
-      {/* <Header /> */}
-      <AddVehicleForm />
-
-    </>
+    <Router>
+      <div className='space-y-3 my-3'>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/add-vehicle" element={<AddVehicleForm />} />
+          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/" element={<div>Welcome to the Home Page</div>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Forms/addVehicleForm.jsx
+++ src/Components/Forms/addVehicleForm.jsx
@@ -1,185 +1,173 @@
-const addVehicleForm = () => {
+const AddVehicleForm = () => {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+    <section className="bg-gray-50 dark:bg-gray-900 py-4">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+        <div className="w-full bg-white rounded-lg shadow dark:border sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Add Vehicle
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4" action="#">
               <div>
                 <label
-                  for="email"
+                  htmlFor="model"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Model
                 </label>
                 <input
                   type="text"
-                  name=""model
+                  name="model"
                   id="model"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="toyota corolla"
-                  required=""
+                  placeholder="Toyota Corolla"
+                  required
                 />
               </div>
               <div>
                 <label
-                  for="name"
+                  htmlFor="name"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Name
                 </label>
                 <input
                   type="text"
-                  name="text"
+                  name="name"
                   id="name"
-                  placeholder=" dodge"
+                  placeholder="Dodge"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  required
                 />
               </div>
-              {/* select category and price on one row on larger devics but responsive on smaller devices */}
-                <div className="flex flex-col md:flex-row md:space-x-4">
-                    <div>
-                    <label
-                        for="category"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Category
-                    </label>
-                    <select
-                        id="category"
-                        name="category"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                    >
-                        <option value="SUV">SUV</option>
-                        <option value="Sedan">Sedan</option>
-                        <option value="Truck">Truck</option>
-                        <option value="Coupe">Coupe</option>
-                    </select>
-                    </div>
-                    <div>
-                    <label
-                        for="price"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Price
-                    </label>
-                    <input
-                        type="text"
-                        name="price"
-                        id="price"
-                        placeholder="10000"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                    />
-                    </div>
-                </div>                
-                {/* description  */}
-                <div>
-                    <label
-                    for="description"
+              <div className="flex flex-col md:flex-row md:space-x-4">
+                <div className="w-full">
+                  <label
+                    htmlFor="category"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                    Description
-                    </label>
-                    <textarea
-                    id="description"
-                    name="description"
-                    placeholder="This is a car"
+                  >
+                    Category
+                  </label>
+                  <select
+                    id="category"
+                    name="category"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required=""
-                    ></textarea>
+                    required
+                  >
+                    <option value="SUV">SUV</option>
+                    <option value="Sedan">Sedan</option>
+                    <option value="Truck">Truck</option>
+                    <option value="Coupe">Coupe</option>
+                  </select>
                 </div>
-                {/* Hp and torque in numeric  in one row  on larger devics and full width on small devices */}
-                <div className="flex flex-col md:flex-row md:space-x-4">
-                    <div>
-                    <label
-                        for="hp"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Horsepower
-                    </label>
-                    <input
-                        type="text"
-                        name="hp"
-                        id="hp"
-                        placeholder="100"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                    />
-                    </div>
-                    <div>
-                    <label
-                        for="torque"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Torque
-                    </label>
-                    <input
-                        type="text"
-                        name="torque"
-                        id="torque"
-                        placeholder="100"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                    />
-                    </div>
+                <div className="w-full">
+                  <label
+                    htmlFor="price"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Price
+                  </label>
+                  <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    placeholder="10000"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
                 </div>
-                {/* engine type and acceleration-sec to 60mp in on row on larger devices */}
-                <div className="flex flex-col md:flex-row md:space-x-4">
-                    <div>
-                    <label
-                        for="engine"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Engine
-                    </label>
-                    <input
-                        type="text"
-                        name="engine"
-                        id="engine"
-                        placeholder="V8"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                    />
-                    </div>
-                    <div>
-                    <label
-                        for="acceleration"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                        Acceleration
-                    </label>
-                    <input
-                        type="text"
-                        name="acceleration"
-                        id="acceleration"
-                        placeholder="5"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                    />
-                    </div>  
+              </div>
+              <div>
+                <label
+                  htmlFor="description"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Description
+                </label>
+                <textarea
+                  id="description"
+                  name="description"
+                  placeholder="This is a car"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                ></textarea>
+              </div>
+              <div className="flex flex-col md:flex-row md:space-x-4">
+                <div className="w-full">
+                  <label
+                    htmlFor="hp"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Horsepower
+                  </label>
+                  <input
+                    type="text"
+                    name="hp"
+                    id="hp"
+                    placeholder="100"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
                 </div>
-                {/* button */}
-                <div>
-                    <button
-                    type="submit"
-                    className="w-full px-4 py-2 text-sm font-medium tracking-wider text-white uppercase bg-primary-600 rounded-lg focus:outline-none focus:ring shadow hover:bg-primary-700 dark:hover:bg-primary-500"
-                    >
-                    Add Vehicle
-                    </button>
+                <div className="w-full">
+                  <label
+                    htmlFor="torque"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Torque
+                  </label>
+                  <input
+                    type="text"
+                    name="torque"
+                    id="torque"
+                    placeholder="100"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
                 </div>
-
-
-
-
- 
-             
-          
+              </div>
+              <div className="flex flex-col md:flex-row md:space-x-4">
+                <div className="w-full">
+                  <label
+                    htmlFor="engine"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Engine
+                  </label>
+                  <input
+                    type="text"
+                    name="engine"
+                    id="engine"
+                    placeholder="V8"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+                <div className="w-full">
+                  <label
+                    htmlFor="acceleration"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Acceleration
+                  </label>
+                  <input
+                    type="text"
+                    name="acceleration"
+                    id="acceleration"
+                    placeholder="5"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full px-4 py-2 text-sm font-medium tracking-wider text-white uppercase bg-primary-600 rounded-lg focus:outline-none focus:ring shadow hover:bg-primary-700 dark:hover:bg-primary-500"
+                >
+                  Add Vehicle
+                </button>
+              </div>
             </form>
           </div>
         </div>
@@ -188,4 +176,4 @@ const addVehicleForm = () => {
   );
 };
 
-export default addVehicleForm;
+export default AddVehicleForm;
