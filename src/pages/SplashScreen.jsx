5c8c449212a4bee7662c82e24fe24cc4fa5bf1ea
fatemanazari <fatemanazari78@gmail.com>
2024-06-07 10:22:23 +0330

    feat: Create splash screen page

---
+++ src/pages/SplashScreen.jsx
@@ -0,0 +1,13 @@
+
+export const SplashScreen = () => {
+  return (
+    <div className="splash-screen">
+      <h1 className='text-4xl text-center mt-24'>Progress Through Technology</h1>
+      <div className='p-2 rounded-3xl max-w-fit flex flex-row space-x-1'>
+      <button className='p-2 bg-blue rounded-tl-3xl rounded-bl-3xl'>Signup</button>
+      <button className='p-2 bg-blue rounded-tr-3xl rounded-br-3xl'>Login</button>
+      </div>
+      
+    </div>
+  )
+}
