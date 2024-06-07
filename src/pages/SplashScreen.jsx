542744b34700b97e0f20576be3760c06f26b68a3
fatemanazari <fatemanazari78@gmail.com>
2024-06-07 18:07:09 +0330

    feat: Create routes for sign up and login buttons

--- src/pages/SplashScreen.jsx
+++ src/pages/SplashScreen.jsx
@@ -1,13 +1,18 @@
+import {useNavigate} from 'react-router-dom';
 
 export const SplashScreen = () => {
+  let navigate = useNavigate();
   return (
-    <div className="splash-screen">
-      <h1 className='text-4xl text-center mt-24'>Progress Through Technology</h1>
-      <div className='p-2 rounded-3xl max-w-fit flex flex-row space-x-1'>
-      <button className='p-2 bg-blue rounded-tl-3xl rounded-bl-3xl'>Signup</button>
-      <button className='p-2 bg-blue rounded-tr-3xl rounded-br-3xl'>Login</button>
+    <>
+      <div className="splash-screen relative">            
+            <div className='p-2 rounded-3xl max-w-fit flex flex-row space-x-1 m-52'>
+              <button onClick={() => (navigate('/signup'))} 
+              className='p-2 border rounded-tl-3xl rounded-bl-3xl text-xl'>Signup</button>
+              <button onClick={() => (navigate('/login')
+              )} 
+              className='p-2 border rounded-tr-3xl rounded-br-3xl text-xl'>Login</button>
+            </div>
       </div>
-      
-    </div>
+    </>
   )
 }
